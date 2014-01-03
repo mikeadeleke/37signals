3ebc7cc6f37c36999d1c0543b231eea9207249e9
Mike Adeleke <madeleke13@gmail.com>
2014-01-03 14:03:08 -0600

    more changes

--- application.js
+++ application.js
@@ -1,21 +1,37 @@
 $(document).ready(function() {
 	$(".right1").hover(function() {
-		$(this).prepend(<img src="img/arrow-right.png" />);
-		$(this "h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
-		$(this "p:first-child").text("");
-	})
+		$(this).mouseenter(function() {
+			$(this).prepend('<img src="img/arrow-right.png" />');
+			$(this).find("h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
+			$(this).find("p:first-child").text("");
+		})
+	}, function() {
+		$(this).mouseleave(function() {
+			$(this).prepend('<img src="img/arrow-right.png" />');
+			$(this).find("h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
+			$(this).find("p:first-child").text("");
+		})
+	}); 
+
+
+	
 
 	$(".right2").hover(function() {
-		$(this).prepend(<img src="img/arrow-right.png" />);
-		$(this "h1:first-child").text("<span>Highrise</span>");
-		$(this "p:first-child").text("");
-	})
+		$(this).prepend('<img src="img/arrow-right.png" />');
+		$(this).find("h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
+		$(this).find("p:first-child").text("");
+	});
 
 	$(".left").hover(function() {
-		$(this).prepend(<img src="img/arrow-left.png" />);
-		$(this "h1:first-child").text("<span>Campfire</span>");
-		$(this "p:first-child").text("");
-	})
-})
+		$(this).prepend('<img src="img/arrow-left.png" />');
+		$(this).find("h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
+		$(this).find("p:first-child").text("");
+	});
+});
 
+// proper way to show hover
+$("#test").hover(function(){} , function(){});
 
+// not sure what this was from...
+<div id="headlines">
+<div id="default" ></div><div id="item1" class="hidden"></div><div id="item2" class"hidden" ></div>
