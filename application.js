8b05b8a24bb72fe83d09c595d09cefb902876f1a
Mike Adeleke <madeleke13@gmail.com>
2014-01-02 18:53:37 -0600

    fixed some jquery and added favicon

--- application.js
+++ application.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-	$(".products").hover(function() {
-		$(".right1").prepend(<img src="arrow-right.png" />);
-		$(".right1 h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
-		$(".right1 p:first-child").text("");
+	$(".right1").hover(function() {
+		$(this).prepend(<img src="arrow-right.png" />);
+		$(this "h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
+		$(this "p:first-child").text("");
 	})
 })
 
 $(document).ready(function() {
-	$(".products").hover(function() {
-		$(".right2").prepend(<img src="arrow-right.png" />);
-		$(".right2 h1:first-child").text("<span>Highrise</span>");
-		$(".right2 p:first-child").text("");
+	$(".right2").hover(function() {
+		$(this).prepend(<img src="arrow-right.png" />);
+		$(this "h1:first-child").text("<span>Highrise</span>");
+		$(this "p:first-child").text("");
 	})
 })
 
 $(document).ready(function() {
-	$(".products").hover(function() {
-		$(".left").prepend(<img src="arrow-left.png" />);
-		$(".left h1:first-child").text("<span>Campfire</span>");
-		$(".left p:first-child").text("");
+	$(".left").hover(function() {
+		$(this).prepend(<img src="arrow-left.png" />);
+		$(this "h1:first-child").text("<span>Campfire</span>");
+		$(this "p:first-child").text("");
 	})
 })
 
