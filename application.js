fcb564bac73222ff4ecc605b6a60f74b10f7f926
Mike Adeleke <madeleke13@gmail.com>
2014-01-02 19:13:51 -0600

    one document ready

--- application.js
+++ application.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
 	$(".right1").hover(function() {
-		$(this).prepend(<img src="arrow-right.png" />);
+		$(this).prepend(<img src="img/arrow-right.png" />);
 		$(this "h1:first-child").text("<span>Basecamp</span> is the project management tool you wish you had on your last project.");
 		$(this "p:first-child").text("");
 	})
-})
 
-$(document).ready(function() {
 	$(".right2").hover(function() {
-		$(this).prepend(<img src="arrow-right.png" />);
+		$(this).prepend(<img src="img/arrow-right.png" />);
 		$(this "h1:first-child").text("<span>Highrise</span>");
 		$(this "p:first-child").text("");
 	})
-})
 
-$(document).ready(function() {
 	$(".left").hover(function() {
-		$(this).prepend(<img src="arrow-left.png" />);
+		$(this).prepend(<img src="img/arrow-left.png" />);
 		$(this "h1:first-child").text("<span>Campfire</span>");
 		$(this "p:first-child").text("");
 	})
 })
+
 
