20362fc34aa73f7dc96e49ca4dd36b8093e07aa1
Mike Adeleke <madeleke13@gmail.com>
2014-01-03 22:05:33 -0600

    finished jquery

--- application.js
+++ application.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
 	$(".box1").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".basecamp").toggleClass("hidden");
+		$(".arrow1").toggleClass("hidden");
 	});
 	$(".box2").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".highrise").toggleClass("hidden");
+		$(".arrow2").toggleClass("hidden");
 	});
 	$(".box3").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".campfire").toggleClass("hidden");
+		$(".arrow3").toggleClass("hidden");
 	});
 });
