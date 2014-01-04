787d1d810dfb3de468e89a4d8292037767663b0f
Mike Adeleke <madeleke13@gmail.com>
2014-01-03 22:19:53 -0600

    done with this for now

--- application.js
+++ application.js
@@ -1,17 +1,30 @@
 $(document).ready(function() {
+	
+	// Happy * insert day * 
+	var today = new Date();
+	var weekdays = new Array("Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday");
+	var day = weekdays[today.getDay()];
+	$('#day').append("Happy "+day+".");
+	// End of happy * insert day *
+
 	$(".box1").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".basecamp").toggleClass("hidden");
 		$(".arrow1").toggleClass("hidden");
 	});
+
 	$(".box2").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".highrise").toggleClass("hidden");
 		$(".arrow2").toggleClass("hidden");
 	});
+	
 	$(".box3").hover(function() {
 		$(".main-event").toggleClass("hidden");
 		$(".campfire").toggleClass("hidden");
 		$(".arrow3").toggleClass("hidden");
 	});
 });
+
+/*--- Day of the Week Animation Section ---*/
+	
